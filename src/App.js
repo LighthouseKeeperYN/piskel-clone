df4ae5bbea392d703930864f7483b2ac0baa43d5
LighthouseKeeperYN <freesaber@yandex.ru>
2019-12-23 22:59:26 +0300

    feat: start setting up environment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+import *component* from '*component path*';
+
+class App extends Component {
+  render() {
+    return (
+      <*component* />
+    );
+  }
+}
+
+export default App;
