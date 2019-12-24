356d3c5d7f5792cd0f81d449f5c5d4054ab85cee
LighthouseKeeperYN <freesaber@yandex.ru>
2019-12-24 17:13:51 +0300

    feat: make navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import './App.scss';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import 'normalize.css';
+import './App.scss';
+import './base.scss';
+
+import Navbar from './components/layout/navbar/Navbar';
+
 const App = () => {
-  // return (
-  // );
+  return (
+    <Fragment>
+      <Navbar />
+      <p>hello world</p>
+    </Fragment>
+  )
 };
 
 export default App;
---
+++ src/components/layout/navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import './navbar.scss';
+
+const Navbar = ({ title, projectName }) => {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar__logo">{title}</h1>
+      <p className="navbar__project-name">{projectName}</p>
+      <div className="navbar__button-wrapper">
+        <a className="navbar__button">Create Sprite</a>
+        <a className="navbar__button">Sign in</a>
+      </div>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: 'Piskel',
+  projectName: 'New Piskel'
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
+
+export default Navbar;
